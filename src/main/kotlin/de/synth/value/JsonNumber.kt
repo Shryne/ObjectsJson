42855e34c927118a579f046b5ec1401429feadc8
= <=>
2020-12-29 19:36:33 +0100

    implement json to JsonNumber conversion

--- src/main/kotlin/de/synth/value/JsonNumber.kt
+++ src/main/kotlin/de/synth/value/JsonNumber.kt
@@ -2,6 +2,8 @@ package de.synth.value
 
 import de.synth.source.JsonSource
 import de.synth.source.ValueSource
+import java.lang.IllegalArgumentException
+import java.lang.NumberFormatException
 import java.util.function.Supplier
 
 /**
@@ -11,12 +13,13 @@ import java.util.function.Supplier
  */
 // TODO: How long is an int/float allowed to be? What about leading zeros?
 class JsonNumber private constructor(
-    private val lazyValue: ValueSource<Number>,
+    private val lazyValue: ValueSource<Number?>,
     private val lazyJson: JsonSource,
     private val lazyIsValid: Supplier<Boolean>
 ) : JsonValue<Number> {
     constructor(value: Number) :
         this(
+            // TODO: implement ValueSource(value). The performance may be better
             ValueSource { value },
             JsonSource { "$value" },
             { true }
@@ -24,12 +27,25 @@ class JsonNumber private constructor(
 
     constructor(json: String) :
         this(
-            TODO("Not yet implemented"),
-            TODO("Not yet implemented"),
-            TODO("Not yet implemented")
+            json.toBigDecimalOrNull().run {
+                if (this == null) {
+                    throw IllegalArgumentException(
+                        "Failed to parse the string ($json) to a number."
+                    )
+                } else {
+                    ValueSource { this }
+                }
+            },
+            JsonSource { json },
+            { true }
         )
 
-    override val value: Number get() = lazyValue.get()
+    override val value: Number get() =
+        lazyValue.get().run {
+            this ?: throw IllegalArgumentException(
+                "Failed to parse the string ($json) to a number."
+            )
+        }
 
     override val isValid: Boolean get() = lazyIsValid.get()
 
